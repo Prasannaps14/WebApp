b733cde6af6ae19cb7bdea52515930ec96f684fa
Prasannaps14 <103628842+Prasannaps14@users.noreply.github.com>
2024-05-18 12:32:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
+const http = require('http');
+ 
+// Create HTTP server
+const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+});
+ 
+// Start server listening on port 3000
+server.listen(8000, () => {
+  console.log('Server Started on port 8000');
+});
+ 
